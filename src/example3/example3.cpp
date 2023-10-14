961d110e0513019f4009de39aecf7e2814c47a62
Tanty102 <nguyentanty102@gmail.com>
2023-10-14 16:05:39 +0700

    [cpp] Example 3

---
+++ src/example3/example3.cpp
@@ -0,0 +1,45 @@
+// Ví dụ xét yêu cầu sau: Viết chương trình nhập toạ độ (x,y) của một dãy điểm, sau đó tìm một điểm cách xa nhau nhất.
+
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+float x[100], y[100];
+float do_dai(int i, int j) 
+{
+    return sqrt(pow(x[i] - x[j], 2) + pow(y[i] - y[j], 2));
+}
+void nhapsl(int n)
+{
+    int i;
+    for (i=1;i<=n;++i)
+    {
+        printf("\n Nhap toa do x,y cua diem thu %d :" ,i);
+        scanf("%f%f",&x[i],&y[i]);
+    }
+}
+int main()
+{
+    int n,i,j,imax,jmax;
+    float d,dmax;
+    printf("\nSo diem N= ");
+    scanf("%d",&n);
+    nhapsl(n);
+    dmax = do_dai(1,2);
+    imax = 1;
+    jmax = 2;
+    for (i=1; i<=n-1; ++i)
+        for(j=i+1;j<=n;++j)
+        {
+            d = do_dai(i,j);
+            if (d > dmax)
+            {
+                dmax = d;
+                imax = i;
+                jmax = j;
+            }
+        }
+        printf("\nDoan thang lon nhat co do dai bang: %0.2f",dmax);
+        printf("\nDi qua 2 diem co chi so la %d va %d:",imax,jmax);
+
+        getch();
+}
