dc253f79a862e9e56e2edf97c562a28233b36e31
Tanty102 <nguyentanty102@gmail.com>
2023-10-15 09:56:45 +0700

    [cpp] Example 5

--- src/example5/example5.cpp
+++ src/example5/example5.cpp
@@ -13,20 +13,26 @@ void main()
     scanf("%d", &n);
     float *x = (float *)malloc((n + 1) * sizeof(float));
     for (int i = 1; i <= n; ++i)
-        {
-            printf("\nX[%d]= ",i);
-            scanf("%f", x + i);
-        }
+    {
+        printf("\nX[%d]= ", i);
+        scanf("%f", x + i);
+    }
     for (i = 1; i <= n - 1; ++i)
+    {
         for (int j = i + 1; j <= n; ++j)
+        {
             if (x[i] > x[j])
             {
                 float tg = x[i];
                 x[i] = x[j];
                 x[j] = tg;
             }
-            printf("\nDay sau khi sap xep\n");
-            for (i = 1; i <= n; ++i)
-                printf("%0.2f", x[i]);
-            getch();
+        }
+    }
+    printf("\nDay sau khi sap xep\n");
+    for (i = 1; i <= n; ++i)
+    {
+        printf("%0.2f", x[i]);
+    }
+    getch();
 }
