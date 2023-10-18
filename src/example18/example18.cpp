8cc502d2538d2c0a7ba536c3d3a93a18fd788e15
Tanty102 <nguyentanty102@gmail.com>
2023-10-18 15:47:36 +0700

    [cpp] Example 18

--- src/example18/example18.cpp
+++ src/example18/example18.cpp
@@ -66,7 +66,7 @@ void main()
     kmax = 3;
     for (i = 1; i<= n - 2; ++i)
     {
-        for (j = i + j; j <= n - 1; ++j)
+        for (j = i + 1; j <= n - 1; ++j)
         {
             for (k = j + 1; k <= n; ++k)
             {
