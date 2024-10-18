78cb12d2b1c6bd4ed75549da14f0567f8e408008
NgocTan <nguyenngoctanpro15@gmail.com>
2024-10-18 23:36:38 +0700

    [issue_002] Fix math

--- learn_php/math.php
+++ learn_php/math.php
@@ -1,31 +1,28 @@
 <!DOCTYPE html>
 <html>
     <body>
-    
             <?php  
-            /*Hàm pi()trả về giá trị PI:*/
+            /*The pi() function returns the value of PI:*/
             echo (pi())."<br>"; 
-            /*Các hàm min()và max()có thể được sử dụng để tìm giá trị thấp nhất hoặc cao nhất trong danh sách đối số:*/
+            /*The min() and max() functions can be used to find the lowest or highest value in a list of arguments:*/
             echo(max(0,200,50,-53,-120)) ."<br>";
             echo(min(0,200,50,-53,-120))."<br>";
-            /*Hàm abs()trả về giá trị tuyệt đối (dương) của một số:*/
+            /*The abs() function returns the absolute (positive) value of a number:*/
             echo(abs(-152))."<br>";
-            /*Hàm sqrt()trả về căn bậc hai của một số:*/
+            /*The sqrt() function returns the square root of a number:*/
             echo(sqrt(64))."<br>";
             echo(sqrt(255))."<br>";
             echo(sqrt(225))."<br>";
-            /*Hàm này round()làm tròn số dấu phẩy động thành số nguyên gần nhất:*/
+            /*The round() function rounds a floating-point number to its nearest integer:*/
             echo(round(12))."<br>";
             echo(round(-1.3))."<br>";
             echo(round(0.49))."<br>";
             echo(round(0.99))."<br>";
-            /*Hàm này rand()tạo ra một số ngẫu nhiên:*/
+            /*The rand() function generates a random number:*/
             echo(rand())."<br>";
             echo(rand())."<br>";
-            /*nếu bạn muốn một số nguyên ngẫu nhiên trong khoảng từ 10 đến 100 (bao gồm cả 10 và 100), hãy sử dụng rand(10, 100):*/
+            /*For example, if you want a random integer between 10 and 100 (inclusive), use rand(10, 100):*/
             echo(rand(0,100))."<br>";
-
-           ?>
-    
+           ?> 
     </body>
 </html>
