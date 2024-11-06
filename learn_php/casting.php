0f556ab76fd6493967139a4b2867f326bf39f500
NgocTan <nguyenngoctanpro15@gmail.com>
2024-11-06 22:39:32 +0700

    [issue_002] Check casting

--- learn_php/casting.php
+++ learn_php/casting.php
@@ -2,14 +2,14 @@
 <html>
     <body>
         <?php 
-        /*To cast to string, use the (string) statement:*/ 
+        //To cast to string, use the (string) statement.
         $x = 1502;
         $y = "15.02";
         $d = "Hello";
         $a = true;
         $b = NULL;
         $x = (string) $x;
-        $y =(string) $y;
+        $y = (string) $y;
         $d = (string) $d;
         $a = (string) $a;
         $b = (string) $b;
@@ -19,7 +19,7 @@
         var_dump($a);
         var_dump($b);
         echo "<br>";
-        /*To cast to integer, use the (int) statement:*/
+        //To cast to integer, use the (int) statement.
         $x = (int) $x;
         $y = (int) $y;
         $d = (int) $d;
@@ -30,8 +30,8 @@
         var_dump($d);
         var_dump($a);
         var_dump($b);
-        echo"<br/>";
-        /*To cast to float, use the (float) statement:*/
+        echo "<br/>";
+        //To cast to float, use the (float) statement.
         $o = "15 Tan";
         $i = "Tan 15";
         $x = (float) $x;
@@ -44,8 +44,8 @@
         var_dump($i);
         var_dump($a);
         var_dump($b);
-        echo"<br/>";
-        /*To cast to boolean, use the (bool) statement:*/
+        echo "<br/>";
+        //To cast to boolean, use the (bool) statement.
         $e = 1233;
         $f = "true";
         $z = "false";
@@ -69,12 +69,12 @@
         var_dump($z);
         var_dump($o);
         var_dump($i);
-        echo"<br/>";
+        echo "<br/>";
         ?>
         </pre>
         <pre>
             <?php
-            /*To cast to array, use the (array) statement:*/
+            //To cast to array, use the (array) statement.
             $h = "Hello World";
             $h = (array) $h;
             var_dump($h);
@@ -86,12 +86,12 @@
             $i = (array) $i;
             $u = (array) $u;
             $s = (array) $s;
-            var_dump ($j);
+            var_dump($j);
             var_dump($i);
             var_dump($u);
             var_dump($s);
             echo "<br>";
-        /*Objects converts into associative arrays where the property names becomes the keys and the property values becomes the values:*/
+        //Objects converts into associative arrays where the property names becomes the keys and the property values becomes the values:
             class Car {
                 public $color;
                 public $model;
@@ -106,8 +106,8 @@
               $myCar = new Car("red", "Volvo");
               $myCar = (array) $myCar;
               var_dump($myCar);
-              echo"<br>";
-        /*To cast to object, use the (object) statement:*/
+              echo "<br>";
+        //To cast to object, use the (object) statement.
               $h = (object) $h;
               $j = (object) $j;
               $i = (object) $i;
@@ -119,14 +119,14 @@
               var_dump($u);
               var_dump($s);
               echo "<br>";
-              /*When converting into objects, most data types converts into a object with one property, named "scalar", with the corresponding value.*/
+              //When converting into objects, most data types converts into a object with one property, named "scalar", with the corresponding value.
             $k = array("Volvo", "BMW", "Toyota"); 
         $l = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43"); 
         $k = (object) $k;
         $l = (object) $l;
         var_dump($l);
         var_dump($k);
-        echo"<br>";
+        echo "<br>";
         ?>
     </body>
 </html>
