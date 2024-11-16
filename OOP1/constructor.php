776c7af9dc1487702769e4a2d2e02199b527770c
NgocTan <nguyenngoctanpro15@gmail.com>
2024-11-16 16:53:03 +0700

    [issue_020] Fix constructor

--- OOP1/constructor.php
+++ OOP1/constructor.php
@@ -3,6 +3,10 @@
 <body>
 
 <?php
+/**
+ * If you create a "__construct()" function, PHP will automatically call this function when you create an object from a class.
+ * Notice that the construct function starts with two underscores "(__)"!
+ */
 class Fruit {
   public $name;
   public $color;
@@ -16,7 +20,28 @@ class Fruit {
 }
 
 $apple = new Fruit("Apple");
+echo $apple->get_name()."<br/>";
+//Another example:
+class Fruit1 {
+  public $name;
+  public $color;
+
+  function __construct($name, $color) {
+    $this->name = $name; 
+    $this->color = $color; 
+  }
+  function get_name() {
+    return $this->name;
+  }
+  function get_color() {
+    return $this->color;
+  }
+}
+
+$apple = new Fruit1("Apple", "red");
 echo $apple->get_name();
+echo "<br>";
+echo $apple->get_color();
 ?>
  
 </body>
