41ce55c5478a006f656e41f737f7db809a8aedf3
NgocTan <nguyenngoctanpro15@gmail.com>
2024-11-18 21:24:35 +0700

    [issue_019] Fix classes

--- OOP/classes.php
+++ OOP/classes.php
@@ -2,6 +2,7 @@
 <html>
     <body>
         <?php
+
         /**
          * In the example below
          * $apple and $banana are instances of the class Fruit.
@@ -9,24 +10,30 @@
         class Fruit11 {
             public $name;
             public $color;
-            //Sets the value of the object's $name property.
 
+            /**
+             * Sets the value of the object's $name property.
+             */
             function set_name($name) {
                 $this->name = $name;
             }
-            //Get the value of the $name property from the object.
+
+            /**
+             * Get the value of the $name property from the object.
+             */
             function get_name() {
                 return $this->name;
             }
         }
         $apple = new Fruit11();
         $banana = new Fruit11();
-        $apple->set_name("Apple"); //Print out "Apple"
-        $banana->set_name("Banana"); //Print out "Banana"
+        $apple->set_name("Apple"); 		//Print out "Apple"
+        $banana->set_name("Banana"); 	//Print out "Banana"
 
         echo $apple->get_name();
         echo "<br/>";
         echo $banana->get_name()."<br/>"; //Print out "Banana"
+
         /**
          * In the example below, 
          * we add two more methods to class Fruit, 
@@ -44,15 +51,24 @@
             function set_name($name) { 
               $this->name = $name;
             }
-            // Get the value of the $name property.
+
+            /**
+             * Get the value of the $name property.
+             */
             function get_name() {
               return $this->name;
             }
-            // Set the value for the $color property.
+
+            /**
+             * Set the value for the $color property.
+             */
             function set_color($color) {
               $this->color = $color;
             }
-            // Get the value of the $color property.
+
+            /**
+             * Get the value of the $color property.
+             */
             function get_color() {
               return $this->color;
             }
@@ -64,13 +80,17 @@
         echo "Name: " . $apple->get_name();
         echo "<br>";
         echo "Color: " .  $apple->get_color()."<br>";
+
         /**
          * So, where can we change the value of the $name property? There are two ways:
          * 1. Inside the class (by adding a set_name() method and use $this):
          */
         class Phone11 {
             public $name;
-            // Sets the value of the object's $name property.
+
+            /**
+             * Sets the value of the object's $name property.
+             */
             function set_name($name) {
               $this->name = $name;
             }
@@ -80,6 +100,7 @@
           
           echo $apple->name;  
           echo "<br>";
+
         //2. Outside the class (by directly changing the property value):
         class Phone1 {
             public $name;
@@ -89,6 +110,7 @@
           
           echo $apple->name;
           echo "<br>";
+
         //You can use the instanceof keyword to check if an object belongs to a specific class.
         class Fruit10 {
             // Properties
@@ -102,7 +124,10 @@
             function set_name($name) {
               $this->name = $name;
             }
-            // Get the value of the $name property.
+
+            /**
+             * Get the value of the $name property.
+             */
             function get_name() {
               return $this->name;
             }
