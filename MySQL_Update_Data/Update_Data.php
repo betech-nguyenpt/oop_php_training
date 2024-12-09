2e66793877b34f0f47c43684d87e5e2ae53912dc
NgocTan <nguyenngoctanpro15@gmail.com>
2024-12-09 21:18:23 +0700

    [issue_104] MySQL Update Data

---
+++ MySQL_Update_Data/Update_Data.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Update Data</title>
+</head>
+<body>
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+/**
+ * The WHERE clause specifies which record or records that should be updated. 
+ * If you omit the WHERE clause, all records will be updated.
+ */
+$sql = "UPDATE MyGuests SET lastname='Tann' WHERE id=1";
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record updated successfully";
+} else {
+  echo "Error updating record: " . $conn->error;
+}
+
+$conn->close();
+?>
+    
+</body>
+</html>
